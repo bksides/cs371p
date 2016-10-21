b7d5df870b808d1e06ceb8341ad854929bab3071
Brandon Sides <bksides@cs.utexas.edu>
2016-10-20 21:08:13 -0500

    Added html documentation

---
+++ projects/allocator/html/search/all_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['allocate',['allocate',['../classAllocator.html#a8a19b7b675f5434c28c68fb15de86f36',1,'Allocator']]],
+  ['allocator',['Allocator',['../classAllocator.html',1,'Allocator&lt; T, N &gt;'],['../classAllocator.html#a4e2bf5fbf94e2206bb72b71ad4b7ffb3',1,'Allocator::Allocator()']]],
+  ['allocator_2eh',['Allocator.h',['../Allocator_8h.html',1,'']]]
+];
---
+++ projects/allocator/html/search/all_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['pointer_5fvalid',['pointer_valid',['../classAllocator.html#a2fdec07c5e123f02349af2f1313aca17',1,'Allocator']]]
+];
